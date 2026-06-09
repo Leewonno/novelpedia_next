96a3659a71e58db04afa2a556664240ca0658079
Leewonno <dldnjssh123@naver.com>
2026-06-10 00:08:17 +0900

    update : novel document ssg delete

--- src/app/(pages)/n/[id]/page.tsx
+++ src/app/(pages)/n/[id]/page.tsx
@@ -1,20 +1,17 @@
-export async function generateStaticParams() {
-  return [
-    { id: "1" },
-    { id: "2" },
-    // 또는 API 호출로 동적 생성
-  ];
-}
-
 export default async function Novel({
   params,
-  // searchParams,
+  searchParams,
 }: {
   params: Promise<{ id: string }>;
-  // searchParams: Promise<{ v?: string }>;
+  searchParams: Promise<{ v?: string }>;
 }) {
   const { id } = await params;
-  // const { v } = await searchParams;
+  const { v } = await searchParams;
 
-  return <div>{id}</div>;
+  return (
+    <>
+      {id}
+      {v && <p>Version: {v}</p>}
+    </>
+  );
 }
