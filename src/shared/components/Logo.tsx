e2bebd621bf1d5255581861476e3bc661fb5071d
Leewonno <dldnjssh123@naver.com>
2026-06-11 23:11:06 +0900

    update : logo priority

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -5,8 +5,8 @@ import logo_default from "@/assets/images/logo-default.png";
 export function Logo() {
   return (
     <div className="flex items-center w-40">
-      <Image src={logo_default} alt="Logo" aria-label="로고" />
-      {/* <Image src={logo_dark} alt="Logo" aria-label="로고" /> */}
+      <Image src={logo_default} alt="Logo" aria-label="로고" priority />
+      {/* <Image src={logo_dark} alt="Logo" aria-label="로고" priority /> */}
     </div>
   );
 }
