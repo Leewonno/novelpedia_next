ac7ad1f2abb7ba0aba58c9550647d15272094658
Leewonno <dldnjssh123@naver.com>
2026-06-16 08:53:05 +0900

    feat : header login link, footer font size

--- src/features/nav/Footer/index.tsx
+++ src/features/nav/Footer/index.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="w-full flex justify-center h-25 border-t border-gray-200">
-      <div className="w-300 h-full flex flex-col justify-center gap-4">
-        <div className="flex gap-4">
+      <div className="w-300 h-full flex flex-col justify-center gap-2">
+        <div className="flex gap-2 text-sm">
           <Link href={"/"}>공지사항</Link>
           <Link href={"/"}>문의</Link>
           <Link href={"/"}>신고</Link>
         </div>
-        <div>ⓒ 2026 NOVELPEDIA. All rights reserved.</div>
+        <div className="text-sm">ⓒ 2026 NOVELPEDIA. All rights reserved.</div>
       </div>
     </footer>
   );
