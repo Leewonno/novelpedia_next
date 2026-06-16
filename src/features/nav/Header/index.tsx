0805bd7398d2ebd2698e99e649f9aea0b2fa5b85
Leewonno <dldnjssh123@naver.com>
2026-06-16 22:43:50 +0900

    fix : header zindex

--- src/features/nav/Header/index.tsx
+++ src/features/nav/Header/index.tsx
@@ -4,7 +4,7 @@ import { Logo } from "@/shared/components/Logo";
 export function Header() {
   const isLogin = false;
   return (
-    <header className="w-full h-14 flex justify-center items-center px-4 border-b border-gray-200">
+    <header className="w-full h-14 flex justify-center items-center px-4 border-b border-gray-200 z-10">
       <div className="w-300 flex justify-between">
         <div>
           <Link href={"/"}>
