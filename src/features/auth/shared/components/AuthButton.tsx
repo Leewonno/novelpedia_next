95ddf173a9270ecca4c2144194c04b6025249611
Leewonno <dldnjssh123@naver.com>
2026-06-18 23:32:32 +0900

    feat : google login button

--- src/features/auth/shared/components/AuthButton.tsx
+++ src/features/auth/shared/components/AuthButton.tsx
@@ -5,13 +5,20 @@ type AuthButtonProps = {
   children?: React.ReactNode;
   type?: "submit" | "kakao" | "google";
   icon?: ReactNode;
+  className?: string;
 };
 
-export function AuthButton({ onClick, children, type, icon }: AuthButtonProps) {
+export function AuthButton({
+  onClick,
+  children,
+  type,
+  icon,
+  className,
+}: AuthButtonProps) {
   return (
     <button
       type={type === "submit" ? "submit" : "button"}
-      className="relative w-full h-11 font-semibold rounded-lg cursor-pointer border border-background-3 outline-brand-glow hover:bg-background-3 text-black transition-colors active:bg-background-0"
+      className={`relative w-full h-11 font-semibold rounded-lg cursor-pointer border border-background-3 outline-brand-glow hover:bg-background-3 text-black transition-colors active:bg-background-0 ${className || ""}!`}
       onClick={onClick}
     >
       <div className="absolute left-3 flex items-center justify-center [&_svg]:size-5 [&_img]:size-5">
